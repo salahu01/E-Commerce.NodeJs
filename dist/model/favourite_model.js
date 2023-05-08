39ec911d34717a862a90ce2720987af642328a19
salahu01 <cvsalahu12@gmail.com>
2023-05-08 15:28:37 +0530

    deleted dist folder

--- dist/model/favourite_model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const mongoose_1 = require("mongoose");
-const favouriteSchema = new mongoose_1.Schema({
-    productIds: {
-        type: Array,
-        required: true,
-    },
-});
-module.exports = (0, mongoose_1.model)("favourite", favouriteSchema);
